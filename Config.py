5b353dc158404e25be765e969e9aade3c91d056d
RasulMuntazar0 <109128514+RasulMuntazar0@users.noreply.github.com>
2022-07-12 08:07:02 +0300

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,13 @@
+import os
+
+class Config(object):
+    API_ID = int(os.environ.get("APP_ID", "6435225"))
+    API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "")
+    HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "JMTHON_ROBOT")
+    SUPPORT = os.environ.get("SUPPORT", "JMTHON_SUPPORT")
+    CHANNEL = os.environ.get("CHANNEL", "JMTHON")
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/03b6a5f345ef6c1c9e6fb.jpg")
+    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/03b6a5f345ef6c1c9e6fb.jpg")
